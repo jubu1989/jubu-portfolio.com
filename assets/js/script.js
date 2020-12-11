96319b93d5929c96c33c00412aa17abb39b3248b
jubu1989 <63472014+jubu1989@users.noreply.github.com>
2020-12-12 01:21:19 +0600

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+// Lightbox init
+var mixer = mixitup('.gallery');
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel();
+  });
+  $('.owl-carousel').owlCarousel({
+      autoplay:true,
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:2
+        }
+    }
+});
+
+// fixed Headers
+
+
+
+
+
+
+
+
+
+    
+    
+
+
+
+
+
+
+
